24c5843a3e7e88c2faa355644d2a954a661fe1e7
santiagon20342 <creature1810@gmail.com>
2024-11-04 13:29:23 -0500

    (day 1 of make a game)

--- Projects/game_1/main.py
+++ Projects/game_1/main.py
@@ -1,1 +1,11 @@
 ##main flie for game 1
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    #draw all elements 
+    #update everthing
+    pygame.display.update()
