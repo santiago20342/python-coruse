b9bfe0906439ec2e1cb93dc39fd966d1b52166fa
santiagon20342 <creature1810@gmail.com>
2024-11-23 12:59:07 -0500

    added color to the screen

--- Projects/game_1/main.py
+++ Projects/game_1/main.py
@@ -1,11 +1,25 @@
 ##main flie for game 1
 import pygame
+from sys import exit 
+
 pygame.init()
 screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('clone trex game')   
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
+
+    screen.blit(test_surface,(0,0))
+
+
     #draw all elements 
     #update everthing
     pygame.display.update()
+    clock.tick(60)
