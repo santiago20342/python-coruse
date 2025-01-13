270ac5827065ab915b45712100221d3219bb6c16
santiagon20342 <creature1810@gmail.com>
2025-01-13 13:34:30 -0500

    finished player class and made obstacle class

--- Projects/game_1/main.py
+++ Projects/game_1/main.py
@@ -6,9 +6,60 @@ from random import randint
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()   
-        self.image = pygame.image.load('graphics\\Player\\player_walk_1.png').convert_alpha()
-        self.rect = self.image.get_rect(midbottom = (200,300))
+        player_walk_1 = pygame.image.load('graphics\\Player\\player_walk_1.png').convert_alpha()
+        player_walk_2 = pygame.image.load('graphics\\Player\\player_walk_2.png').convert_alpha()
+        self.player_walk = [player_walk_1,player_walk_2]
+        self.player_jump = pygame.image.load('graphics\\Player\\jump.png').convert_alpha()
+        self.player_index = 0
 
+
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom = (200,300))
+        self.gravity = 0
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+            self.gravity = -20
+    
+    def apply_gravity(self):
+        self.gravity += 1 
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300 
+    
+    def animation_state(self):
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk): self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()   
+        self.animation_state() 
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+        
+        if type == 'fly':
+            fly_1 = pygame.image.load('graphics\\fly\\fly1.png').convert_alpha()
+            fly_2 = pygame.image.load('graphics\\fly\\fly2.png').convert_alpha()
+            self.frames = [fly_1, fly_2]
+            y_pos = 210
+        else:
+            snail_1 = pygame.image.load('graphics\\snail\\snail1.png').convert_alpha()
+            snail_2 = pygame.image.load('graphics\\snail\\snail2.png').convert_alpha()
+            self.frames = [snail_1, snail_2]
+            y_pos = 300
+        
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (randint(900,1100),y_pos))
 
 
 
@@ -167,6 +218,7 @@ while True:
         player_animation()
         screen.blit(player_surf,player_rect)
         player.draw(screen)
+        player.update()
 
         #obstacle movement 
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
